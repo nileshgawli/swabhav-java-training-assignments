51421159c763aea14acd8cd0f166f3e1181b8be8
nileshgawli <nbgawli@gmail.com>
2025-07-16 15:55:45 +0530

    add: LineItem model

---
+++ shopping_cart_app/src/com/aurionpro/model/LineItem.java
@@ -0,0 +1,32 @@
+package com.aurionpro.model;
+
+public class LineItem {
+	private int id;
+	private int quantity;
+	private Product product;
+	
+	public LineItem(int id, int quantity, Product product) {
+		super();
+		this.id = id;
+		this.quantity = quantity;
+		this.product = product;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public double calculateLineItemCost() {
+		return product.calculateDiscountedPrice() * quantity;
+	}
+	
+	
+}
